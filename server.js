c2017406c3d1f36ca425ca1b256ea429852c0d52
Monte <monte6198@gmail.com>
2023-03-24 02:26:51 +0900

    feat: DB 연결 및 User 스키마 생성

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+import { DATABASE } from './config.js';
 
 const app = express();
 
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(DATABASE)
+  .then(() => {
+    console.log('***** MongoDB connected. *****');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.listen(8000, () => {
-  console.log('***********Server running on port 8000***********');
+  console.log('*********** Server running on port 8000 ***********');
 });
